c7435739209cb52963042ee75c78bc9fa608393b
elj40 <elaijoubert@gmail.com>
2025-01-12 18:52:43 +0200

    Little docs for utils.h

--- utils.h
+++ utils.h
@@ -12,11 +12,15 @@ struct LL_Node
 
 typedef struct LL_Node LL_Node;
 
+// Array operations
 int ArrayMax(int nums[], int n);
 float ArrayMaxf(float nums[], int n);
+
+// Misc
 int nearlyEqual(float a, float b);
 void printInt(const void * i);
 
+// Generic linked list operations
 void LL_push(LL_Node ** head, void * data);
 LL_Node * LL_remove(LL_Node ** head, void * data);
 void LL_print(LL_Node * head, void (*printFunc)(const void *) );

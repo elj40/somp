1c74caff506c91745c8aeef668cfea72837368ec
elj40 <elaijoubert@gmail.com>
2025-03-10 17:55:22 +0200

    elnob can build somp now

--- elnob.c
+++ elnob.c
@@ -1,33 +1,65 @@
 #define ELNOB_IMPLEMENTATION
 #include "elnob.h"
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdbool.h>
 
+bool build_somp()
+{
+    // gcc -ggdb -o "somp.exe" somp.c somp_logic.c utils.c -L%SDL_PATH_LIB% -I%SDL_PATH_INCLUDE% -lmingw32 -lSDL2main -lSDL2 
+    // somp.exe
+#define SDL_PATH_INCLUDE "/home/eli/Software/thirdparty/SDL-release-2.30.12/include/" 
+    printf("-I"SDL_PATH_INCLUDE"\n");
+    const char * compile[] = { "gcc", "-ggdb", "-o", "somp", "somp.c", "somp_logic.c", "utils.c", "-I"SDL_PATH_INCLUDE, "-lSDL2main", "-lSDL2", NULL }; 
+    if (!run_command_sync(ELNOB_ARRAY_SIZE(compile), compile)) return false;
+    const char * run[] = { "./somp", NULL };
+    if (!run_command_sync(ELNOB_ARRAY_SIZE(run), run)) return false;
+
+    return true;
+}
+
+bool build_tests()
+{
+    const char * compile[] = { "gcc","-Wall","-Wextra","-ggdb","-o","tester","somp_tester.c","-lm", NULL };
+    if (!run_command_sync(ELNOB_ARRAY_SIZE(compile), compile)) return false;
+    const char * run[] = { "./tester", NULL };
+    if (!run_command_sync(ELNOB_ARRAY_SIZE(run), run)) return false;
+
+    return true;
+}
 int main(int argc, const char * argv[])
 {
-	printf("Hello somp!\n");	
-	// This will be sync compile
 
-	// Compile
-	pid_t pid = fork();
-	if (pid == 0)
-	{
-		int r = execlp("gcc", "gcc", "-Wall", "-Wextra", "-ggdb", "-o", "tester", "somp_tester.c", "-lm", NULL);
-		if (r < 0) return 1;
-		return 0;
-	} 
+    struct stat code_stat;
+    struct stat libh_stat;
+    struct stat exec_stat;
+	stat("elnob.c", &code_stat);
+	stat("elnob.h", &libh_stat);
+	stat("elnob.out", &exec_stat);
 
-	wait(NULL);
-	printf("This should run after compilation\n");
+    if (code_stat.st_mtime > exec_stat.st_mtime
+        || libh_stat.st_mtime > exec_stat.st_mtime)
+    {
+        printf("=========== ELNOB RECOMPILE =============\n");
+        const char * compile[] = {"gcc", "-o", "elnob.out", "elnob.c", NULL};
+        if (!run_command_sync(ELNOB_ARRAY_SIZE(compile), compile)) return 2;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		int r = execlp("./tester", "./tester");
-		if (r < 0) return 1;
-		return 0;
-	}
+        const char * rerun[] = {"./elnob.out",NULL};
+        if (!run_command_sync(ELNOB_ARRAY_SIZE(rerun), rerun)) return 2;
+        return 0;
+    }
 
-	return 0;
+    printf("============= ELNOB RUN =================\n");
+
+    if (argc > 1 && strcmp(argv[1], "test") == 0)
+    {
+        if (!build_tests()) return 1;
+    } else 
+    {
+        if (!build_somp()) return 1;
+    }
+
+
+    return 0;
 }
--- elnob.h
+++ elnob.h
@@ -1,9 +1,60 @@
 #ifndef _ELNOB_H_
 #define _ELNOB_H_
 
-// So far there are no functions but here is a short example
+#define ELNOB_ARRAY_SIZE(a) sizeof((a))/sizeof((a)[0])
+int run_command_sync(int argc, const char * argv[]);
 
-// =================== EXAMPLE ====================
+#ifdef ELNOB_IMPLEMENTATION
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h> 
+
+// argc includes the NULL at the end
+int run_command_sync(int argc, const char * argv[])
+{
+	if (argc <= 0) {
+		printf("Command must have at least one argument\n");
+		return 0;
+	}
+	if (argv[argc-1] != NULL) {
+		printf("Command %s must be terminated by NULL\n", argv[0]);
+		return 0;
+	}
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		printf("Running custom command: %s\n", argv[0]);
+		int r = execvp(argv[0], (char * const *) argv);
+		if (r < 0) {
+            printf("Failed to run command: %s\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+	}
+
+    int exit_status = 0;
+	wait(&exit_status);
+
+    //printf("exit_status: %d %d\n", exit_status, WEXITSTATUS(exit_status));
+    if (WEXITSTATUS(exit_status) != 0)
+    {
+        printf("ERROR: failed to run command: %s, exiting\n", argv[0]);
+        return 0;
+    }
+    return 1;
+}
+#endif // ELNOB_IMPLEMENTATION
+#endif // _ELNOB_H_
+
+
+// NOTE!!! Examples are made as elnob is developed, 
+// so some may stop working at some point
+
+// =================== EXAMPLE 1 ====================
+// Pure C, no abstraction
+//
 // int main()
 //{
 //	printf("Hello Elnob\n");
@@ -11,22 +62,55 @@
 //
 //	if (pid == 0)
 //	{
-//		int result = execlp("gcc", "gcc", "-Wall", "-Wextra", "-o", "elnob", "elnob.c", NULL);
-//		if (result < 0) { printf("Failed to run command\n"); return 1; }
+//		int result = execlp("gcc", "gcc", "-Wall", "-Wextra", "-o",
+//		"elnob", "elnob.c", NULL);
+//		if (result < 0) { 
+//			printf("Failed to run command\n"); 
+//			return 1; 
+//		}
 //		return 0;
 ////	} else
 //	{
-//		wait(NULL);2
+//		wait(NULL);
 //		printf("I want this to run as well\n");
 //	}
-//
 //	return 0;
 //}
-// =================================================
 
-#ifdef ELNOB_IMPLEMENTATION
-#include <unistd.h>
-#include <sys/wait.h> 
-#endif // ELNOB_IMPLEMENTATION
+// =================== EXAMPLE 2 ====================
+// Using run_command
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
 
-#endif // _ELNOB_H_
+#define ELNOB_IMPLEMENTATION
+#include "elnob.h"
+
+// NOTE, we get away with wait(NULL) because we make sure that we do not fork
+// more than once, a more secure function would be better
+int main()
+{
+	struct stat code_stat;
+	struct stat exec_stat;
+	stat("elnob.c", &code_stat);
+	stat("elnob.out", &exec_stat);
+
+	pid_t pid;
+
+	if (code_stat.st_mtime > exec_stat.st_mtime)
+	{
+		printf("elnob.c edited, need to recompile\n");
+		const char * compile[] = {"gcc", "-o", "elnob.out", "elnob.c", NULL};
+		run_command(4, compile);
+
+		const char * rerun[] = {"./elnob.out", NULL};
+		run_command(1,  rerun);
+		return 0;
+	}
+	const char * cmd[] = {"ls", "-a", NULL};
+	run_command(2, cmd);
+	return 0;
+}
+*/

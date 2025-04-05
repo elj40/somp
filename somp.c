b1484dcba43b5439645b33469cd2f4b2ae31f54a
elj40 <elaijoubert@gmail.com>
2025-04-05 02:45:42 +0200

    Set up for getting user input

--- somp.c
+++ somp.c
@@ -28,30 +28,49 @@
 #define SOMP_LOGIC_IMPLEMENTATION
 #include "somp_logic.h"
 
+void read_beam_info(Beam * beam);
+void read_pointforce_info(PointForce pfs[]);
+void read_distributedforce_info(DistributedForce dfs[]);
+
+void read_beam_info(Beam * beam) {}
+void read_pointforce_info(PointForce pfs[]) {}
+void read_distributedforce_info(DistributedForce dfs[]) {}
+
 int main(int argc, char * argv[])
 {
 	printf("Hello SOMP\n");
 
+    // Get beam data from user
+    //      beam length
+    //      point forces
+    //      distributed forces
 	Beam beam = {0};
 	beam.length = 1.0;
 	beam.sectionsCount = MAX_SECTIONS;
 
-	PointForce pointForces[10]; //TODO: figure out how many we should allocate for
+	PointForce point_forces[10]; //TODO: figure out how many we should allocate for
 	int pfCount;
-	pointForces[0] = (PointForce){ 0.0, 1 };
-	pointForces[1] = (PointForce){ 0.25,2 };
-	pointForces[2] = (PointForce){ 0.5, 3 };
-	pointForces[3] = (PointForce){ 1.0, 4 };
+	point_forces[0] = (PointForce){ 0.0, 1 };
+	point_forces[1] = (PointForce){ 0.25,2 };
+	point_forces[2] = (PointForce){ 0.5, 3 };
+	point_forces[3] = (PointForce){ 1.0, 4 };
 	pfCount = 4;
 
-	DistributedForce distributedForces[10];
+	DistributedForce distributed_forces[10];
 	int dfCount;
-	distributedForces[0] = (DistributedForce){ 0, 0.5, {1,0} };
-	distributedForces[1] = (DistributedForce){ 0.25, 0.75, {2,0} };
-	distributedForces[2] = (DistributedForce){ 0.75, 1.0, {3,0} };
-	distributedForces[3] = (DistributedForce){ 0.65, 0.95, {4,0} };
+	distributed_forces[0] = (DistributedForce){ 0, 0.5, {1,0} };
+	distributed_forces[1] = (DistributedForce){ 0.25, 0.75, {2,0} };
+	distributed_forces[2] = (DistributedForce){ 0.75, 1.0, {3,0} };
+	distributed_forces[3] = (DistributedForce){ 0.65, 0.95, {4,0} };
 	dfCount = 4;
-	solveBeam(&beam, pointForces, pfCount, distributedForces, dfCount);
+
+    read_beam_info(&beam);
+    read_pointforce_info(point_forces);
+    read_distributedforce_info(distributed_forces);
+    // Solve beam
+	solveBeam(&beam, point_forces, pfCount, distributed_forces, dfCount);
+
+    // Ouput beam results
 
 	return 0;
 }
